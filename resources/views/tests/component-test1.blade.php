2ecac7d9a97857de41164c0a6a42ee7356207202
Murota <ryosuke.murota123@gmail.com>
2022-07-17 11:56:27 +0900

    props

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -2,5 +2,7 @@
     <x-slot name="header">ヘッダー１</x-slot>
 コンポーネントテスト１
 
-    <x-tests.card title="タイトル1" content="本文1" />
+    <x-tests.card title="タイトル1" content="本文1" :message="$message" />
+    <x-tests.card title="タイトル2" />
+    <x-tests.card />
 </x-tests.app>
