6fbc686b5a8f4c11d29fca305c61f813b82dc64f
Murota <ryosuke.murota123@gmail.com>
2022-07-20 00:15:42 +0900

    リソースコントローラ

--- app/Models/Owner.php
+++ app/Models/Owner.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Owner extends uthenticatable
+class Owner extends Authenticatable
 {
     use HasFactory;
 
