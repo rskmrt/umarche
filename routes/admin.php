34f09dca6cc43a409f313c763237c62dc03839c2
Murota <ryosuke.murota123@gmail.com>
2022-07-20 21:53:21 +0900

    Carbon

--- routes/admin.php
+++ routes/admin.php
@@ -33,6 +33,8 @@ Route::get('/dashboard', function () {
     return view('admin.dashboard');
 })->middleware(['auth:admin'])->name('dashboard');
 
+
+
 Route::middleware('guest')->group(function () {
     Route::get('register', [RegisteredUserController::class, 'create'])
                 ->name('register');
