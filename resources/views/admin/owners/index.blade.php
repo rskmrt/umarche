f1d1b029ff7780db82a26d08e7f8e02a8d65a796
Murota <ryosuke.murota123@gmail.com>
2022-07-28 21:18:12 +0900

    Store

--- resources/views/admin/owners/index.blade.php
+++ resources/views/admin/owners/index.blade.php
@@ -8,9 +8,10 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white bor
+                <div class="p-6 bg-white border">
                     <section class="text-gray-600 body-font">
                         <div class="container px-5 mx-auto">
+                            <x-flash-message status="info" />
                             <div class="flex justify-end mb-4">
                               	<button onclick="location.href='{{ route('admin.owners.create') }}'" class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">新規登録する</button>
                             </div>
@@ -18,28 +19,26 @@
 								<table class="table-auto w-full text-left whitespace-no-wrap">
 									<thead>
 										<tr>
-										<th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">名前</th>
-										<th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">email</th>
-										<th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">作成日</th>
-										<th class="w-10 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tr rounded-br"></th>
+                                            <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">名前</th>
+                                            <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">email</th>
+                                            <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">作成日</th>
+                                            <th class="w-10 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tr rounded-br"></th>
 										</tr>
 									</thead>
 									@foreach ($owners as $owner)
 									<tbody>
 										<tr>
-										<td class="px-4 py-3">{{ $owner->name }}</td>
-										<td class="px-4 py-3">{{ $owner->email }}</td>
-										<td class="px-4 py-3">{{ $owner->created_at->diffForHumans() }}</td>
-										<td class="w-10 text-center">
-											<input name="plan" type="radio">
-										</td>
+                                            <td class="px-4 py-3">{{ $owner->name }}</td>
+                                            <td class="px-4 py-3">{{ $owner->email }}</td>
+                                            <td class="px-4 py-3">{{ $owner->created_at->diffForHumans() }}</td>
+                                            <td class="w-10 text-center"><input name="plan" type="radio"></td>
 										</tr>
 									</tbody>
 									@endforeach
 								</table>
-                          </div>
+                            </div>
                         </div>
-                      </section>
+                    </section>
                     
                     {{-- エロクアント
                     @foreach ($e_all as $e_owner)
